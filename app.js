db7a23fb41e75140cfa0e6311f7b1eacdf31e71b
ryan schillreff <29GSchillreff@live.ytech.edu>
2026-03-11 09:25:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,36 +8,38 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
+app.get('/game.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/game.html'))
+
+})
+app.get('/party.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/party.html'))
+
+})
 
 app.post('/submitpage', (req, res) => {
-    console.log(req.body.id);
-    // Get the data ready
-const payload = {
-    from: req.body.from,
-    to: 90,
-    amount: 50,
-    reason: 'Play Party Crashers',
-    pin: req.body.pin
-};
-    // Make the transfer request
-fetch(`${FORMBAR_ADDRESS}/api/digipogs/transfer`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(payload),
-}).then((transferResult) => {
-    return transferResult.json();
-}).then((responseData) => {
-    console.log(responseData.success);
-    if (responseData.success) {
-        res.send("done")
-        
-    } else {
-        res.send("ERROR: " + responseData.message)
-        
-    }
-    // responseData will be: { success: true/false, message: "..." }
+    const payload = {
+        from: req.body.from,
+        to: 1,
+        amount: 1,
+        reason: 'Play Party Crashers',
+        pin: req.body.pin
+    };
+
+    fetch(`${FORMBAR_ADDRESS}/api/digipogs/transfer`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+    })
+        .then((transferResult) => transferResult.json())
+        .then((responseData) => {
+            if (responseData.success) {
+                res.redirect('/game.html');
+            } else {
+                res.send("ERROR: " + responseData.message);
+            }
+        });
 });
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
